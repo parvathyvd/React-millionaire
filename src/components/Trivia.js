1690f84ac26ae5925f21ec556889d2b14f50cc37
Parvathy VD <paru@Parvathys-MacBook-Pro.local>
2022-06-27 10:26:17 -0700

    React who wants to a be a millionaire game project

---
+++ src/components/Trivia.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+
+const Trivia = ({ data, setStop, questionNum, setQuestionNum }) => {
+  const [question, setQuestion] = useState(null);
+  const [selectAnswer, setSelectAnswer] = useState(null);
+  const [className, setClassName] = useState("answer");
+
+  useEffect(() => {
+    setQuestion(data[questionNum - 1]);
+  }, [data, questionNum]);
+  const delay = (duration, cb) => {
+    setTimeout(() => {
+      cb();
+    }, duration);
+  };
+  const onClickHandler = (ans) => {
+    console.log(ans);
+    setSelectAnswer(ans);
+    setClassName("answer active");
+    delay(2000, () => {
+      setClassName(ans.correct ? "answer correct" : "answer wrong");
+    });
+
+    delay(6000, () => {
+      if (ans.correct) {
+        setQuestionNum((prev) => prev + 1);
+        setSelectAnswer(null);
+      } else {
+        setStop(true);
+      }
+    });
+  };
+
+  return (
+    <div className="trivia">
+      <div className="question">{question?.question}</div>
+      <div className="answers">
+        {question?.answers.map((ans, index) => {
+          return (
+            <div
+              className={selectAnswer === ans ? className : "answer"}
+              key={index}
+              onClick={() => onClickHandler(ans)}
+            >
+              {ans.text}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Trivia;
