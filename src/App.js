0037e90ca293ab62773f2208745b06aaed3bc096
Parvathy VD <paru@Parvathys-MacBook-Pro.local>
2022-06-27 11:05:57 -0700

    added 2 more questions- a max of 5

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   const [questionNum, setQuestionNum] = useState(1);
   const [stop, setStop] = useState(false);
   const [earned, setEarned] = useState("$0");
+  const [endOfQuestion, setEndOfQuestion] = useState(false);
 
   const data = [
     {
@@ -75,6 +76,52 @@ function App() {
         },
       ],
     },
+    {
+      id: 4,
+      question:
+        "Which Disney character famously leaves a glass slipper behind at a royal ball?",
+      answers: [
+        {
+          text: "Sleeping Beauty",
+          correct: false,
+        },
+        {
+          text: "Else",
+          correct: false,
+        },
+        {
+          text: "Ariel",
+          correct: false,
+        },
+        {
+          text: "Cinderella",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 5,
+      question:
+        "Which of these brands was chiefly associated with the manufacture of household locks?",
+      answers: [
+        {
+          text: "Phillips",
+          correct: false,
+        },
+        {
+          text: "Flymo",
+          correct: false,
+        },
+        {
+          text: "Chubb",
+          correct: true,
+        },
+        {
+          text: "Sony",
+          correct: false,
+        },
+      ],
+    },
   ];
 
   const moneyPyramid = useMemo(() => {
@@ -83,24 +130,27 @@ function App() {
       { id: 2, amount: "$ 200" },
       { id: 3, amount: "$ 300" },
       { id: 4, amount: "$ 500" },
-      { id: 5, amount: "$ 1.000" },
-      { id: 6, amount: "$ 2.000" },
-      { id: 7, amount: "$ 4.000" },
-      { id: 8, amount: "$ 8.000" },
-      { id: 9, amount: "$ 16.000" },
-      { id: 10, amount: "$ 32.000" },
-      { id: 11, amount: "$ 64.000" },
-      { id: 12, amount: "$ 125.000" },
-      { id: 13, amount: "$ 250.000" },
-      { id: 14, amount: "$ 500.000" },
-      { id: 15, amount: "$ 1.000.000" },
+      { id: 5, amount: "$ 1000" },
+      { id: 6, amount: "$ 2000" },
+      { id: 7, amount: "$ 4000" },
+      { id: 8, amount: "$ 8000" },
+      { id: 9, amount: "$ 16000" },
+      { id: 10, amount: "$ 32000" },
+      { id: 11, amount: "$ 64000" },
+      { id: 12, amount: "$ 125000" },
+      { id: 13, amount: "$ 250000" },
+      { id: 14, amount: "$ 500000" },
+      { id: 15, amount: "$ 1000000" },
     ].reverse();
   }, []);
 
   useEffect(() => {
     questionNum > 1 &&
       setEarned(moneyPyramid.find((m) => m.id === questionNum - 1).amount);
-  }, [questionNum, moneyPyramid]);
+    if (endOfQuestion) {
+      setEarned(moneyPyramid.find((m) => m.id === questionNum).amount);
+    }
+  }, [questionNum, moneyPyramid, endOfQuestion]);
 
   return (
     <div className="app">
@@ -111,7 +161,12 @@ function App() {
           <>
             <div className="top">
               <div className="timer">
-                <Timer setStop={setStop} questionNum={questionNum} />
+                <Timer
+                  setStop={setStop}
+                  questionNum={questionNum}
+                  earned={earned}
+                  endOfQuestion={endOfQuestion}
+                />
               </div>
             </div>
             <div className="bottom">
@@ -120,6 +175,9 @@ function App() {
                 setStop={setStop}
                 questionNum={questionNum}
                 setQuestionNum={setQuestionNum}
+                endOfQuestion={endOfQuestion}
+                setEndOfQuestion={setEndOfQuestion}
+                earned={earned}
               />
             </div>
           </>
--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -1,13 +1,24 @@
 import React, { useEffect, useState } from "react";
 
-const Trivia = ({ data, setStop, questionNum, setQuestionNum }) => {
+const Trivia = ({
+  data,
+  setStop,
+  questionNum,
+  setQuestionNum,
+  earned,
+  setEndOfQuestion,
+  endOfQuestion,
+}) => {
   const [question, setQuestion] = useState(null);
   const [selectAnswer, setSelectAnswer] = useState(null);
   const [className, setClassName] = useState("answer");
 
   useEffect(() => {
+    if (questionNum > 5) {
+      setEndOfQuestion(true);
+    }
     setQuestion(data[questionNum - 1]);
-  }, [data, questionNum]);
+  }, [data, questionNum, setEndOfQuestion]);
   const delay = (duration, cb) => {
     setTimeout(() => {
       cb();
@@ -33,20 +44,29 @@ const Trivia = ({ data, setStop, questionNum, setQuestionNum }) => {
 
   return (
     <div className="trivia">
-      <div className="question">{question?.question}</div>
-      <div className="answers">
-        {question?.answers.map((ans, index) => {
-          return (
-            <div
-              className={selectAnswer === ans ? className : "answer"}
-              key={index}
-              onClick={() => onClickHandler(ans)}
-            >
-              {ans.text}
-            </div>
-          );
-        })}
-      </div>
+      {endOfQuestion ? (
+        <p className="endQuestion">
+          You can add more question to play more. Right now it's ended. You have
+          earned {earned}
+        </p>
+      ) : (
+        <>
+          <div className="question">{question?.question}</div>
+          <div className="answers">
+            {question?.answers.map((ans, index) => {
+              return (
+                <div
+                  className={selectAnswer === ans ? className : "answer"}
+                  key={index}
+                  onClick={() => onClickHandler(ans)}
+                >
+                  {ans.text}
+                </div>
+              );
+            })}
+          </div>
+        </>
+      )}
     </div>
   );
 };
